3a7f16df62b249a8c7677594b45928f6478714ad
Antoine Fillet <momentumcorporatefinance@gmail.com>
2019-11-13 16:56:21 +0100

    restaureview

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   resources :restaurants do
+     resources :reviews, only: [ :new, :create ]
+    end
+  # A visitor can see the list of all restaurants.
+  # GET "restaurants"
+  # get "restaurants", to: "restaurants#index"
+  # # She/He can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # # GET "restaurants/new"
+  # # POST "restaurants"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # # She/He can see the details of a restaurant, with all the reviews related to the restaurant.
+  # # GET "restaurants/38"
+  # # NB: The `show` route needs to be *after* `new` route.
+  # get "restaurants/:id", to: "restaurants#show"
+
+  # # She/He can see the details of a restaurant, with all the reviews related to the restaurant.
+  # # GET "restaurants/38"
+  # get "restaurants/:restaurant_id/reviews", to: "reviews#index"
+  # # She/He can add a new review to a restaurant
+  # # GET "restaurants/38/reviews/new"
+  # # POST "restaurants/38/reviews"
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  # post "restaurants/:restaurant_id/reviews",    to: "reviews#create"
+  # # She/He can see the details of a restaurant, with all the reviews related to the restaurant.
+  # # GET "restaurants/38"
+  # get "reviews/:id", to: "reviews#show"
+  # get "reviews/:id/edit", to: "reviews#edit"
 end
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Royal Moceau',
+    address:      '37 Avenue Hoche, 75008 Paris',
+    phone_number:  '01 42 99 88 00',
+    category:        'french'
+  },
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '00 44 207 26395',
+    category:        'italian'
+  },
+  {
+    name:         'Oriental',
+    address:      '1245 New Road, Bangkok 200766',
+    phone_number:  '00 207 457 76678',
+    category:        'japanese'
+  },
+  {
+    name:         'Zhong guo',
+    address:      'Tian an men street, Beijing 109',
+    phone_number:  '00 34 45677 664.',
+    category:        'chinese'
+  },
+  {
+    name:         'Sayonara',
+    address:      'Ginza , Tokyo Japan',
+    phone_number:  '00 45 7688 998',
+    category:        'japanese'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
